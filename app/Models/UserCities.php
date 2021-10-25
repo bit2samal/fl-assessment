0474bc6617bea93d2ed29c3f3b6fcc927b0f7c23
Anil kumar <b2k8786@gmail.com>
2021-10-25 19:32:36 +0530

    get functions in user cities model

--- app/Models/UserCities.php
+++ app/Models/UserCities.php
@@ -9,11 +9,16 @@ class UserCities extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['user_id', 'city', 'is_default'];
+    protected $fillable = ['user_id', 'city', 'state', 'country', 'is_default'];
 
-    public static function getCities(int $user_id): object
+    public static function getCities(int $user_id): ?object
     {
-        return UserCities::select('id', 'city')->where('user_id', $user_id)->get();
+        return UserCities::select('id', 'city', 'state', 'country')->where('user_id', $user_id)->get();
+    }
+
+    public static function getCity(int $city_id): ?object
+    {
+        return UserCities::select('id', 'city', 'state', 'country')->where('id', $city_id)->first();
     }
 
     public static function removeCity(int $user_id, int $city_id): int
